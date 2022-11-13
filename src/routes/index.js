a973298b1d20b59918824312ac1439194318b446
Gesiel Neves <neves.gesiel@gmail.com>
2022-11-13 18:36:57 -0300

    Add validation

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,39 +7,91 @@ router.get("/", (req, res) => {
 });
 
 router.get("/list-items", async (req, res) => {
-  const items = await ListItem.find();
-  return res.json(items);
+  try {
+    const items = await ListItem.find();
+    return res.json(items);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
 });
 
 router.post("/list-item", async (req, res) => {
-  const newItems = await ListItem.create({
-    name: req.body.name,
-    quantity: req.body.quantity,
-    checked: req.body.checked,
-  });
-  return res.json(newItems);
+  try {
+    const { name, quantity, checked } = req.body;
+    if (!name || name.length < 3) {
+      return res
+        .status(400)
+        .json({ error: "Name must be at least 3 characters" });
+    }
+
+    if (!quantity || typeof quantity !== "number") {
+      return res.status(400).json({
+        error: "Quantity must be a number",
+      });
+    }
+
+    const newItems = await ListItem.create({
+      name,
+      quantity,
+      checked: checked || false,
+    });
+    return res.json(newItems);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
 });
 
 router.delete("/list-item/:id", async (req, res) => {
-  const id = req.params.id;
-  const listItemDeleted = await ListItem.findByIdAndDelete(id);
-  return res.json(listItemDeleted);
+  try {
+    const id = req.params.id;
+    if (!id) {
+      return res.status(400).json({
+        error: "Id is mandatory",
+      });
+    }
+    const listItemDeleted = await ListItem.findByIdAndDelete(id);
+    return res.json(listItemDeleted);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
 });
 
 router.put("/list-item/:id", async (req, res) => {
-  const id = req.params.id;
-  const listItemUpdated = await ListItem.findByIdAndUpdate(
-    id,
-    {
-      name: req.body.name,
-      quantity: req.body.quantity,
-      checked: req.body.checked,
-    },
-    {
-      new: true,
+  try {
+    const id = req.params.id;
+    if (!id) {
+      return res.status(400).json({
+        error: "Id is mandatory",
+      });
     }
-  );
-  return res.json(listItemUpdated);
-});
 
+    const { name, quantity, checked } = req.body;
+    if (!name || name.length < 3) {
+      return res
+        .status(400)
+        .json({ error: "Name must be at least 3 characters" });
+    }
+
+    if (!quantity || typeof quantity !== "number") {
+      return res.status(400).json({
+        error: "Quantity must be a number",
+      });
+    }
+
+    const listItemUpdated = await ListItem.findByIdAndUpdate(
+      id,
+      {
+        name,
+        quantity,
+        checked: checked || false,
+      },
+      {
+        new: true,
+      }
+    );
+    return res.json(listItemUpdated);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+});
 module.exports = router;
